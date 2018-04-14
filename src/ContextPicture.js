62b0c81e99c598e320eefe6e47eb7ae305cb55bf
b018167a <noble.ninja@gmail.com>
2018-04-14 08:40:18 +0100

    border radius on image

--- src/ContextPicture.js
+++ src/ContextPicture.js
@@ -2,18 +2,12 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const Picture = styled.img`
+  border-radius: 4px 4px 0 0;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
   width: 100%;
 `;
 
 class ContextPicture extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      img: ""
-    };
-  }
-
   render() {
     return (
       <Picture
